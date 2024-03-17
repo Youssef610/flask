ed4efad59862349754ad3cc2d7b75926d3bf7fab
usfnassar <77360510+Youssef610@users.noreply.github.com>
2024-03-17 02:42:53 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,58 @@
+from flask import Flask, request, jsonify
+from login import loginPage
+from natiga import get_natiga
+from register import get_register
+
+app = Flask(__name__)
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    try:
+        data = request.get_json()
+        id = data['id']
+        code = data['code']
+        extracted_data = loginPage(id, code)
+    except Exception as e:
+        error_message = "Internal Server Error: {}".format(str(e))
+        return jsonify({"error": error_message}), 500
+
+    return jsonify(extracted_data)
+
+
+@app.route('/natiga', methods=['POST'])
+def natiga():
+    try:
+        data = request.get_json()
+        ID = data['id']
+        code = data['code']
+        extracted_data = get_natiga(ID, code)
+    except Exception as e:
+        error_message = "Internal Server Error: {}".format(str(e))
+        return jsonify({"error": error_message}), 500
+
+    return extracted_data
+
+
+@app.route('/register', methods=['POST'])
+def register():
+    try:
+        print('WE IN REGISTER')
+        data = request.get_json()
+        ID = data['id']
+        code = data['code']
+        extracted_data = get_register(code=code, ID=ID)
+    except Exception as e:
+        error_message = "Internal Server Error: {}".format(str(e))
+        return jsonify({"error": error_message}), 500
+    return extracted_data
+
+
+@app.route('/')
+def home():
+    return jsonify({"msg": "Server For Our SCI APP..."})
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
