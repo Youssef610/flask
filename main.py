7ed0f0c92919896d652588d85bf95ecb9c97b723
usfnassar <77360510+Youssef610@users.noreply.github.com>
2024-03-18 02:00:11 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 from login import loginPage
 from natiga import get_natiga
 from register import get_register
+from regist_sup import regist_sup
 import asyncio
 
 app = Flask(__name__)
@@ -16,6 +17,10 @@ async def async_natiga(ID, code):
 
 async def async_register(ID, code):
     return await asyncio.to_thread(get_register, ID, code)
+
+async def async_regist_sup(ID, code,indexOFSub):
+    return await asyncio.to_thread(regist_sup, ID, code,indexOFSub)
+
 @app.route('/login', methods=['POST'])
 def login():
     try:
@@ -56,10 +61,25 @@ def register():
         error_message = "Internal Server Error: {}".format(str(e))
         return jsonify({"error": error_message}), 500
     return extracted_data
+@app.route('/registSup', methods=['POST'])
+def regisSup():
+    try:
+        print('WE IN RegistSup')
+        data = request.get_json()
+        ID = data['id']
+        code = data['code']
+        indexOFSub = data['indexOFSub']
+        extracted_data = regist_sup(code=code, ID=ID,indexOFSub=indexOFSub)
+    except Exception as e:
+        error_message = "Internal Server Error: {}".format(str(e))
+        return jsonify({"error": error_message}), 500
+    return extracted_data
 
 
 @app.route('/')
 def home():
     return jsonify({"msg": "Server For Our SCI APP..."})
+
+
 
 
