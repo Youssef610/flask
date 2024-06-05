3dc769019cad2f9a4ef84c4ae7c46f70e70d19f1
usfnassar <77360510+Youssef610@users.noreply.github.com>
2024-06-05 23:05:57 +0300

    Update ai.py

--- ai.py
+++ ai.py
@@ -7,31 +7,63 @@ url = "http://pass-gpt.nowtechai.com/api/v1/pass"
 
 
 def ai(msg):
-    payload = json.dumps({
-        "contents": [
-            {
-                "role": "system",
-                "content": "You are SCI bot, a large language model trained by sci team."
-            },
-            {
-                "role": "user",
-                "content": msg
-            }
-        ]
-    })
 
-    headers = {
-        'User-Agent': "Ktor client",
-        'Connection': "Keep-Alive",
-        'Accept': "application/json",
-        'Accept-Encoding': "gzip",
-        'Content-Type': "application/json",
-        'Key': "ya/kfyhrEWBBIq6jysr2PnTXCD1gMyvXQI4B4DrykMYKenfZeKmPw0x+kgSC/XbpUctoYk0GtwLnr8nfWs25Bw==",
-        'TimeStamps': "1717601117827",
-        'Accept-Charset': "UTF-8"
-    }
-    response = requests.post(url, data=payload, headers=headers)
-    json_strings = re.findall(r'data:({.*?})', response.text)
-    res = "".join(json.loads(
-        js)["content"] for js in json_strings if json.loads(js)["status"] == "stream")
-    return jsonify({"aiResponse": res})
+
+
+  url = "https://chatgpt-au.vulcanlabs.co/api/v1/token"
+
+  payload = json.dumps({
+    "device_id": "57C8AC7D51346473",
+    "order_id": "",
+    "product_id": "",
+    "purchase_token": "",
+    "subscription_id": ""
+  })
+
+  headers = {
+    'User-Agent': "Chat Smith Android, Version 3.6.2(548)",
+    'Accept': "application/json",
+    'Accept-Encoding': "gzip",
+    'Content-Type': "application/json",
+    'x-vulcan-application-id': "com.smartwidgetlabs.chatgpt",
+    'x-vulcan-request-id': "9149487891713168904149"
+  }
+  while True:
+    try:
+      response = requests.post(url, data=payload, headers=headers,timeout=5)
+      break
+    except:
+      continue
+
+
+  auth=response.json()["AccessToken"]
+
+  url = "https://prod-smith.vulcanlabs.co/api/v6/chat"
+  payload = json.dumps({
+    "model": "gpt-3.5-turbo",
+    "user": "57C8AC7D51346473",
+    "messages": [
+      {
+        "role": "system",
+        "content": "You are Chat Smith, a personal AI. Your words are never longer than 2000 words,if asked for result say look to botoom if english word but if arabic say  انظر للاسفل  ."    },
+      {
+        "role": "user",
+        "content":f"{msg}"
+      }
+    ],
+    "nsfw_check": True
+  })
+
+  headers = {
+    'User-Agent': "Chat Smith Android, Version 3.6.2(548)",
+    'Accept': "application/json",
+    'Accept-Encoding': "gzip",
+    'Content-Type': "application/json",
+    'x-firebase-appcheck': "AppCheck",
+    'authorization': f"Bearer {auth}",
+    'x-vulcan-application-id': "com.smartwidgetlabs.chatgpt",
+    'x-vulcan-request-id': "9149487891713142621674"
+  }
+
+  response = requests.post(url, data=payload, headers=headers)
+  return jsonify({"aiResponse": response.json()["choices"][0]["Message"]["content"]})
